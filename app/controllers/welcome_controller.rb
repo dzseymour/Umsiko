df6fb3d69cab9b6b1adef1397cb10ac4a601f6b3
Dez Seymour <409Montrose@gmail.com>
2018-07-01 09:39:07 +1200

    db stuff
    added in postgresql db

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,49 @@
 class WelcomeController < ApplicationController
-  def index
+  
+
+  def getTitle
+     'Umsiko'
   end
+
+  def getEmail
+    'UmsikoNZ@gmail.com'
+  end
+
+  def getName
+    'Iona Woolgrove'
+  end
+
+  def getHome
+    'Home'
+  end
+
+  def getAboutUs
+    'About us'
+  end
+
+  def getServices
+    'Services'
+  end
+
+  def getClients
+    'Clients'
+  end
+
+  def getSignature
+    '© PlotPoint: ' + Date.today.year.to_s
+  end
+
+  def index
+    @Title = getTitle
+    @Email = getEmail
+    @Name = getName
+    @Home = getHome
+    @AboutUs = getAboutUs
+    @Services = getServices
+    @Clients = getClients
+    @Signature = getSignature
+  end
+
+   
+   	
 end
